cccef8d122af17a5a8215e871dc1e9897e16b19c
Shubham Gandhi <sg9769356@gmail.com>
2024-07-28 19:17:28 +0530

    add trending topics hashtags

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import { PostEditor } from "@/components/posts/editor/PostEditor";
 import { Post } from "@/components/posts/Post";
+import { TrendsSideBar } from "@/components/TrendsSideBar";
 import prisma from "@/lib/prisma";
 import { postDataInclude } from "@/lib/types";
 
@@ -10,16 +11,15 @@ export default async function Home() {
       createdAt: "desc",
     },
   });
-  console.log(posts);
-
   return (
-    <main className="h-[200vh] w-full">
+    <main className="flex h-[200vh] w-full gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
+      <TrendsSideBar />
     </main>
   );
 }
